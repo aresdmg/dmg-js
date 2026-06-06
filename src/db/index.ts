bab2968cef05850479321d7b4db33460ec224df4
Ares <thisisares01@gmail.com>
2026-06-06 22:37:51 +0530

    feat: database added

---
+++ src/db/index.ts
@@ -0,0 +1,19 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+export async function ConnectDB() {
+    const dbUrl = process.env.DATABASE_URL
+    if (!dbUrl) {
+        throw new Error("DATABASE_URL not set")
+    }
+
+    const pool = new Pool({
+        connectionString: dbUrl,
+        max: 20,
+        idleTimeoutMillis: 30_000,
+        connectionTimeoutMillis: 20_000
+    })
+
+    const db = drizzle(pool)
+    return { db, pool }
+}
---
+++ src/db/schema.ts
@@ -0,0 +1,22 @@
+import { uuid, pgTable, varchar, text, boolean, timestamp } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    fullName: varchar("full_name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    password: text("password"),
+    avatar: varchar("avatar", { length: 255 }).notNull().default("https://res.cloudinary.com/desamhhkj/image/upload/v1774217400/avatar0_jntvgv.jpg"),
+    isNew: boolean("is_new").default(true),
+    createdAt: timestamp("created_at", { withTimezone: true, mode: "date" }).defaultNow().notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true, mode: "date" }).defaultNow().notNull(),
+    deletedAt: timestamp("deleted_at", { withTimezone: true, mode: "date" })
+});
+
+export const tokensTable = pgTable("tokens", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    userId: uuid("user_id").references(() => usersTable.id, { onDelete: "cascade" }).notNull(),
+    refreshToken: varchar("refresh_token", { length: 255 }).notNull(),
+    revoked: boolean("revoked").default(false).notNull(),
+    expiredAt: timestamp("expired_at", { withTimezone: true, mode: "date" }).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true, mode: "date" }).defaultNow().notNull()
+})
